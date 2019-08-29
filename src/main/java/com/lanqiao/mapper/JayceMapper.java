a9c74f0aec117f32cdf8845e82c4fff78de231bb
wph <1315306351@qq.com>
2019-08-29 16:11:06 +0800

    “昆明路口”

--- src/main/java/com/lanqiao/mapper/JayceMapper.java
+++ src/main/java/com/lanqiao/mapper/JayceMapper.java
@@ -1,10 +1,12 @@
 package com.lanqiao.mapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.lanqiao.domain.Commodity;
+import com.lanqiao.domain.Wen;
 
 @Mapper
 public interface JayceMapper {
@@ -16,4 +18,12 @@ public interface JayceMapper {
 	public int[] SearchILike(int uid);
 	
 	public List<Commodity> SearchSameCom(int tid);
+	
+	public ArrayList<Wen> SearchWen(int uid);
+	
+	public List<Commodity> SelectAllCom();
+	
+	public void insertWen(Wen wen);
+	
+	public Wen SearchWenByWen(Wen wen);
 }
